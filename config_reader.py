779a38d6244cd1841c8f5daccbdf4ae05018ed66
alex-p-pigeon <alex.p.pigeon@gmail.com>
2026-04-14 16:02:03 +0400

    Polish portfolio presentation: remove stale docs and legacy config
    - Delete handlers/learnpath/README.md (outdated, references deleted files,
      garbled encoding)
    - config_reader.py: remove Russian comments, remove unused YC_ACCOUNT_ID /
      YC_SECRET_KEY fields
    - .env.example: remove Yandex Cloud legacy section

--- config_reader.py
+++ config_reader.py
@@ -6,12 +6,7 @@ from pathlib import Path
 
 class Settings(BaseSettings):
 
-
-    # Желательно вместо str использовать SecretStr 
-    # для конфиденциальных данных, например, токена бота
     bot_token: SecretStr
-    YC_ACCOUNT_ID: SecretStr
-    YC_SECRET_KEY: SecretStr
     DB_USER: SecretStr
     DB_PASSWORD: SecretStr
     DB_PORT: SecretStr
@@ -29,28 +24,17 @@ class Settings(BaseSettings):
     URL_AGR: ClassVar[str] = "https://pigeoncorner.github.io/tg_app_lingo/index.html?page=agreement"
     URL_TZ: ClassVar[str] = "https://pigeoncorner.github.io/tg_app_lingo/index.html?page=timezone"
     URL_HELP: ClassVar[str] = "https://pigeoncorner.github.io/tg_app_lingo/index.html?page=help"
-    #URL_WORD_SELECTOR: ClassVar[str] = "https://pigeoncorner.github.io/tg_app_lingo/index.html?page=wordselector"
 
-
-
-    # Начиная со второй версии pydantic, настройки класса настроек задаются
-    # через model_config
-    # В данном случае будет использоваться файла .env, который будет прочитан
-    # с кодировкой UTF-8
     if os.name == 'nt':
         font_path: ClassVar[str] = r"C:\Windows\Fonts\constan.ttf"
         model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-
     else:
         script_dir: ClassVar[Path] = Path(__file__).parent
-        model_config = SettingsConfigDict(env_file=str(script_dir / 'env.env'), env_file_encoding='utf-8') #'~/lingo/env.env'
+        model_config = SettingsConfigDict(env_file=str(script_dir / 'env.env'), env_file_encoding='utf-8')
         font_path: ClassVar[str] = "/usr/share/fonts/truetype/vista/constan.ttf"
 
 
 
-# При импорте файла сразу создастся 
-# и провалидируется объект конфига, 
-# который можно далее импортировать из разных мест
 config = Settings()
 
 
